759c8746c5642aceaf2bda63e40bb99150aad40d
flxgn <12431629+flxgn@users.noreply.github.com>
2022-01-10 20:14:55 +0100

    Rename Event to Msg
    to reflect that it could be an event or a command

--- src/processor/sliding_average.rs
+++ src/processor/sliding_average.rs
@@ -15,15 +15,15 @@ pub struct PriceUpdated<'a> {
 }
 
 #[derive(Debug, PartialEq, Clone)]
-pub enum Event<'a> {
+pub enum Msg<'a> {
     LivePriceUpdated(PriceUpdated<'a>),
     AveragePriceUpdated(PriceUpdated<'a>),
     Shutdown,
 }
 
 pub struct Processor<'a> {
-    pub input: channel::Receiver<Event<'a>>,
-    pub output: channel::Sender<Event<'a>>,
+    pub input: channel::Receiver<Msg<'a>>,
+    pub output: channel::Sender<Msg<'a>>,
     pub is_filter: bool,
     pub window_millis: i64,
 
@@ -40,13 +40,14 @@ impl<'a> Processor<'a> {
         loop {
             let e = self.input.recv().expect("open channel");
             match &e {
-                Event::Shutdown => break,
-                Event::LivePriceUpdated(e) => {
+                Msg::Shutdown => break,
+                Msg::LivePriceUpdated(e) => {
                     self.events.push(TimePricePoint {
                         datetime: e.datetime,
                         price: e.price,
                     });
-                    self.events.retain(|i| i.datetime >= e.datetime - self.window_millis as i64 );
+                    self.events
+                        .retain(|i| i.datetime >= e.datetime - self.window_millis as i64);
                     let sum: f64 = self.events.iter().map(|e| e.price).sum();
                     let avg = PriceUpdated {
                         pair_id: e.pair_id,
@@ -56,7 +57,7 @@ impl<'a> Processor<'a> {
                     };
                     if self.events.len() > 1 {
                         self.output
-                            .send(Event::AveragePriceUpdated(avg))
+                            .send(Msg::AveragePriceUpdated(avg))
                             .expect("open channel");
                     }
                 }
@@ -82,8 +83,8 @@ mod tests {
         is_filter: bool,
     ) -> (
         Processor<'a>,
-        channel::Sender<Event<'a>>,
-        channel::Receiver<Event<'a>>,
+        channel::Sender<Msg<'a>>,
+        channel::Receiver<Msg<'a>>,
     ) {
         let (in_s, in_r) = unbounded();
         let (out_s, out_r) = unbounded();
@@ -103,7 +104,7 @@ mod tests {
     #[test]
     fn processor_should_exit_if_shutdown_received() {
         let (processor, in_s, _) = new_processor(0, false);
-        in_s.send(Event::Shutdown).unwrap();
+        in_s.send(Msg::Shutdown).unwrap();
         processor.start();
         assert!(true);
     }
@@ -111,11 +112,11 @@ mod tests {
     #[test]
     fn processor_should_output_events_if_not_filtered() {
         let (processor, in_s, out_r) = new_processor(0, false);
-        let expected_e = Event::LivePriceUpdated(PriceUpdated {
+        let expected_e = Msg::LivePriceUpdated(PriceUpdated {
             ..Default::default()
         });
         in_s.send(expected_e.clone()).unwrap();
-        in_s.send(Event::Shutdown).unwrap();
+        in_s.send(Msg::Shutdown).unwrap();
         processor.start();
         let actual_e = out_r.recv().unwrap();
         assert_eq!(expected_e, actual_e);
@@ -124,13 +125,13 @@ mod tests {
     #[test]
     fn processor_should_emit_average_price_update() {
         let (processor, in_s, out_r) = new_processor(SECOND, true);
-        let e1 = Event::LivePriceUpdated(PriceUpdated {
+        let e1 = Msg::LivePriceUpdated(PriceUpdated {
             pair_id: "pair_id",
             datetime: 0,
             price: 1.0,
             ..Default::default()
         });
-        let e2 = Event::LivePriceUpdated(PriceUpdated {
+        let e2 = Msg::LivePriceUpdated(PriceUpdated {
             pair_id: "pair_id",
             datetime: SECOND,
             price: 2.0,
@@ -138,11 +139,11 @@ mod tests {
         });
         in_s.send(e1).unwrap();
         in_s.send(e2).unwrap();
-        in_s.send(Event::Shutdown).unwrap();
+        in_s.send(Msg::Shutdown).unwrap();
         processor.start();
 
         let actual_e = out_r.recv().unwrap();
-        let expected_e = Event::AveragePriceUpdated(PriceUpdated {
+        let expected_e = Msg::AveragePriceUpdated(PriceUpdated {
             pair_id: "pair_id",
             datetime: SECOND,
             price: 1.5,
@@ -154,17 +155,17 @@ mod tests {
     #[test]
     fn processor_should_calculate_prices_from_given_sliding_window() {
         let (processor, in_s, out_r) = new_processor(SECOND, true);
-        let e1 = Event::LivePriceUpdated(PriceUpdated {
+        let e1 = Msg::LivePriceUpdated(PriceUpdated {
             datetime: 0,
             price: 1.0,
             ..Default::default()
         });
-        let e2 = Event::LivePriceUpdated(PriceUpdated {
+        let e2 = Msg::LivePriceUpdated(PriceUpdated {
             datetime: SECOND,
             price: 2.0,
             ..Default::default()
         });
-        let e3 = Event::LivePriceUpdated(PriceUpdated {
+        let e3 = Msg::LivePriceUpdated(PriceUpdated {
             datetime: SECOND * 2,
             price: 3.0,
             ..Default::default()
@@ -172,11 +173,11 @@ mod tests {
         in_s.send(e1).unwrap();
         in_s.send(e2).unwrap();
         in_s.send(e3).unwrap();
-        in_s.send(Event::Shutdown).unwrap();
+        in_s.send(Msg::Shutdown).unwrap();
         processor.start();
 
         let actual_e1 = out_r.recv().unwrap();
-        let expected_e1 = Event::AveragePriceUpdated(PriceUpdated {
+        let expected_e1 = Msg::AveragePriceUpdated(PriceUpdated {
             datetime: SECOND,
             price: 1.5,
             ..Default::default()
@@ -184,7 +185,7 @@ mod tests {
         assert_eq!(expected_e1, actual_e1);
 
         let actual_e2 = out_r.recv().unwrap();
-        let expected_e2 = Event::AveragePriceUpdated(PriceUpdated {
+        let expected_e2 = Msg::AveragePriceUpdated(PriceUpdated {
             datetime: SECOND * 2,
             price: 2.5,
             ..Default::default()
